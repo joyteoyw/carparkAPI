db762df102b936e1dc6ff842666aac6487c11e2b
joyteoyw <67196052+joyteoyw@users.noreply.github.com>
2024-11-28 09:40:00 +0800

    switch to gunicorn for deployment

--- carparkapi.py
+++ carparkapi.py
@@ -62,4 +62,4 @@ def not_found_error(error):
     return Response(json.dumps({"response": "error", "error": str(error)}), mimetype="application/json", status=404)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(debug=True)
